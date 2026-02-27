1507d69ce130414d323461e43463763bcd2599f1
OPatrolin <o.patrolin@bdx.ecolescreatives.com>
2026-02-27 16:59:41 +0100

    deplacement et saut
    deplacement gauche a droite et saut simple, murs

---
+++ Stellar/Assets/Scripts/Chara_Cntrl.cs
@@ -0,0 +1,113 @@
+using Unity.VisualScripting;
+using UnityEngine;
+public class Chara_Cntrl : MonoBehaviour
+
+
+{
+    /* // V1 Code deplacement
+    [Header("Move variables")]
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float acceleration = 20f;
+
+    [Header("Gravity/Jump")]
+    [SerializeField] float gravity = -10f;
+    [SerializeField] float jumpHeight = 1.5f;
+
+    Rigidbody2D rb;
+    Vector2 MyInput;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+
+    void Update()
+    {
+        MyInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        MyInput.Normalize();
+
+
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = MyInput * moveSpeed;
+    }
+    */
+
+
+
+    [Header("Move variables")]
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float acceleration = 20f;
+
+    [Header("Gravity/Jump")]
+    [SerializeField] float gravity = -10f;
+    [SerializeField] float jumpForce = 5f;
+    [SerializeField] float holdTime = 50f;
+
+
+    Rigidbody2D rb;
+    float inputX;
+    public LayerMask groundLayer;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+
+
+    void Update()
+    {
+        inputX = Input.GetAxisRaw("Horizontal");
+
+        bool isGrounded = Physics2D.Raycast(transform.position, Vector2.down, 1.1f, groundLayer);
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+
+            /*if (Input.GetButtonDown("Jump") && isGrounded)
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce * holdTime);
+
+
+
+            }
+            */
+
+        }
+
+
+
+
+
+    }
+
+    void FixedUpdate()
+    {
+        var v = rb.linearVelocity;
+        v.x = inputX * moveSpeed;
+
+        rb.linearVelocity = v;
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
