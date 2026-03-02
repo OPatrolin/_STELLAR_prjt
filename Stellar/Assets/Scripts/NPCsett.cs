188900411a899fa56d679a19844d038604a62e5f
OPatrolin <o.patrolin@bdx.ecolescreatives.com>
2026-03-02 16:11:20 +0100

    npc present
    juste present sans contact, passer devant

--- Stellar/Assets/Scripts/NPCsett.cs
+++ Stellar/Assets/Scripts/NPCsett.cs
@@ -15,5 +15,21 @@ public class NPCsett : MonoBehaviour
     void Update()
     {
         bool isGrounded = Physics2D.Raycast(transform.position, Vector2.down, 1.1f, groundLayer);
+
     }
+
+  /*  void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.name);
+
+        if (collision.gameObject.name.Contains("Player"))
+        {
+            Debug.Log("contact");
+           // collision.gameObject.//afficher ui proposer interaction
+
+        }
+
+
+    }
+  */
 }
