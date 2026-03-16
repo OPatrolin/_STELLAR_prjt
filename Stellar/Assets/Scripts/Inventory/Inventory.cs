1c7e6d281ae9e40e07067c309c691452fde6806e
OPatrolin <o.patrolin@bdx.ecolescreatives.com>
2026-03-16 17:07:55 +0100

    inventaire deplacement
    changement place icon et image dans l'inventaire

--- Stellar/Assets/Scripts/Inventory/Inventory.cs
+++ Stellar/Assets/Scripts/Inventory/Inventory.cs
@@ -1,5 +1,9 @@
-using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using UnityEngine.Windows;
 
 public class Inventory : MonoBehaviour
 {
@@ -10,9 +14,14 @@ public class Inventory : MonoBehaviour
     public GameObject hotbarObj;
     public GameObject inventorySlotParent;
 
+    public UnityEngine.UI.Image dragIcon;
+
     private List<Slot> inventorySlots = new List<Slot>();
     private List<Slot> hotbarSlots = new List<Slot>();
     private List<Slot> allslots = new List<Slot>();
+
+    private Slot draggedSlot = null;
+    private bool isDragging = false;
 
 
      void Awake()
@@ -22,19 +31,23 @@ public class Inventory : MonoBehaviour
 
         allslots.AddRange(inventorySlots);
         allslots.AddRange(hotbarSlots);
-
     }
 
      void Update()
     {
-        if(Input.GetKeyDown(KeyCode.W))
+        if(UnityEngine.Input.GetKeyDown(KeyCode.W))
         {
             AddItem(woodItem, 3);
         }
-        else if (Input.GetKeyDown(KeyCode.G))
+        else if (UnityEngine.Input.GetKeyDown(KeyCode.G))
         {
             AddItem(axeItem, 1);
         }
+
+
+        StartDrag();
+        UpdateDragItemPosition();
+        EndDrag();
     }
 
 
@@ -75,21 +88,115 @@ public class Inventory : MonoBehaviour
 
 
                 if (remanining <= 0) return;
-
             }
 
-            if (remanining> 0) Debug.Log("Inventory is full, could not add " + remanining + " of " + itemtoAdd.ItemName);
-        
+           // if (remanining> 0) Debug.Log("Inventory is full, could not add " + remanining + " of " + itemtoAdd.ItemName);
+        }
+    }
+
+
+    private void StartDrag()
+    {
+        if (UnityEngine.Input.GetMouseButtonDown(0))
+        {
+            Slot hovered = GetHoveredSlot();
+
+            if (hovered != null && hovered.HasItem())
+            {
+                draggedSlot = hovered;
+                isDragging = true;
+
+                // show drag item
+                dragIcon.sprite = hovered.GetItem().icon;
+                dragIcon.color = new Color(1, 1, 1, 0.5f);
+                dragIcon.enabled = true;
+            }
+        }
+    }
+
+    private void EndDrag()
+    {
+        Slot hovered = GetHoveredSlot();
+
+        if(UnityEngine.Input.GetMouseButtonUp(0) && isDragging)
+        {
+            Slot hoverd = GetHoveredSlot();
+
+            if (hovered != null)
+            {
+                HandleDrop(draggedSlot, hovered);
+
+                dragIcon.enabled = false;
+
+                draggedSlot = null;
+                isDragging = false;
+            }
+        }
+    }
+
+
+    private Slot GetHoveredSlot()
+    {
+        foreach(Slot s in allslots)
+        {
+            if (s.hovering)
+                return s;
         }
 
-
-
-
-
-
-
-
-
-
+        return null;
     }
+
+
+   
+    private void HandleDrop(Slot from, Slot to)
+    {
+        if (from == to) return; 
+
+        // Stacking
+        if(to.HasItem() && to.GetItem() == from.GetItem())
+        {
+            int max = to.GetItem().maxStackSize;
+            int space = max - to.GetAmount();
+
+            if(space > 0)
+            {
+                int move = Mathf.Min(space, from.GetAmount());
+
+                to.SetItem(to.GetItem(), to.GetAmount() + move);
+                from.SetItem(from.GetItem(), from.GetAmount() - move);
+
+                if (from.GetAmount() <= 0)
+                    from.ClearSlot();
+
+                return;
+            }
+        }
+
+        //Diff Item
+        if(to.HasItem())
+        {
+            itemS0 tempItem = to.GetItem();
+            int tempAmount = to.GetAmount();
+
+            to.SetItem(from.GetItem(), from.GetAmount());
+            from.SetItem(tempItem, tempAmount);
+            return;
+        }
+
+        //Empty Slot
+        to.SetItem(from.GetItem(), from.GetAmount());
+        from.ClearSlot();
+    }
+
+    private void UpdateDragItemPosition()
+    {
+      if(isDragging)
+        {
+            dragIcon.transform.position = UnityEngine.Input.mousePosition;
+        }
+    }
+
+
+
+
 }
